53f71f32787974057aa50a86bc0975d91770d046
Jeffrey <vandenbornejeffrey@gmail.com>
2014-04-21 17:40:32 +0200

    added icons, gtk icon cache update, grub2-set-default policy

--- src/extension.js
+++ src/extension.js
@@ -5,14 +5,44 @@ const St = imports.gi.St;
 
 const PanelMenu = imports.ui.panelMenu;
 const Main = imports.ui.main;
+const PopupMenu = imports.ui.popupMenu;
+const Panel = imports.ui.panel;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
+const GrubLoader = Me.imports.grubLoader;
 
 const Gettext = imports.gettext.domain('gnome-shell-extension-grub');
 const _ = Gettext.gettext;
 const N_ = function(x) { return x; }
+
+const ICON_SIZE = 16;
+
+const GrubMenuItem = Lang.Class({
+    Name: 'GrubMenuItem',
+    Extends: PopupMenu.PopupBaseMenuItem,
+
+    _init: function (info) {
+        this.parent();
+        this._info = info;
+
+        if (info.icon) {
+            this._icon = info.icon;
+            this.actor.add_child(this._icon);
+        }
+
+        this._label = new St.Label({text: info.alias});
+        this.actor.add_child(this._label);
+    },
+    destroy: function () {
+        this.parent();
+    },
+    activate: function (event) {
+        this._info.launch(event.get_time());
+        this.parent(event);
+    }
+});
 
 const GrubMenu = new Lang.Class({
     Name: 'GrubMenu.GrubMenu',
@@ -21,18 +51,33 @@ const GrubMenu = new Lang.Class({
     _init: function () {
         this.parent(0.0, _("Grub Boot Menu"));
         let hbox = new St.BoxLayout({ style_class: 'panel-status-menu-box' });
-        let label = new St.Label({ text: _("Grub Boot Menu"),
-                                   y_expand: true,
-                                   y_align: Clutter.ActorAlign.CENTER });
-        hbox.add_child(label);
-        hbox.add_child(new St.Label({ text: '\u25BE',
-                                      y_expand: true,
-                                      y_align: Clutter.ActorAlign.CENTER }));
-        this.actor.add_actor(hbox);
+        let icon = new St.Icon({ icon_name: 'system-restart',
+                             style_class: 'system-status-icon' });
+        this.actor.add_actor(icon);
+        this._sections = {};
+        this.grubLoader = new GrubLoader.GrubLoader();
+
+        // Create menu
+        this._create();
     },
 
     destroy: function () {
         this.parent();
+    },
+    _create: function () {
+        let entries = this._loadMenuEntries();  
+        let id = 'menu-entries'
+        this._sections[id] = new PopupMenu.PopupMenuSection();
+
+        for (let i = 0; i < entries.length; ++i) {
+            this._sections[id].addMenuItem(new GrubMenuItem(entries[i]));
+        }
+
+        this._sections[id].actor.visible = entries.length > 0;
+        this.menu.addMenuItem(this._sections[id]);
+    },
+    _loadMenuEntries: function () {
+        return this.grubLoader.getEntries();
     }
 });
 
@@ -44,8 +89,7 @@ let _indicator;
 
 function enable() {
     _indicator = new GrubMenu;
-    let pos = 1;
-    Main.panel.addToStatusArea('grub-boot-menu', _indicator, pos, 'left');
+    Main.panel.addToStatusArea('grub-boot-menu', _indicator);
 }
 
 function disable() {
---
+++ src/grubLoader.js
@@ -0,0 +1,112 @@
+const Lang = imports.lang;
+const Shell = imports.gi.Shell;
+const St = imports.gi.St;
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const Signals = imports.signals;
+const Util = imports.misc.util;
+const MessageTray = imports.ui.messageTray;
+const Main = imports.ui.main;
+const GnomeSession = imports.misc.gnomeSession;
+
+const GrubLoader = new Lang.Class({
+    Name: 'GrubLoader',
+
+    _init: function () {
+        this._entries = [];
+    },
+    getEntries: function () {
+        let cfgFile = Gio.file_new_for_path('/boot/grub2/grub.cfg');
+        let size = cfgFile.query_info(
+                "standard::size",
+                Gio.FileQueryInfoFlags.NONE,
+                null).get_size();
+
+        let stream = cfgFile.read(null);
+        let data = stream.read_bytes(size, null).get_data();
+        stream.close(null);
+
+        return this._cfgToEntriesArray(data);
+    },
+    _cfgToEntriesArray: function (data) {
+        let entries = [];
+
+        data = String(data);
+        let mEntryRegex = /^\s*menuentry\s+\'(.*?)\'/gm;
+        let matchArray;
+        while ((matchArray = mEntryRegex.exec(data)) !== null) {
+            let eName = matchArray[1];
+            let icon = this._findIcon(eName);
+
+            // TODO: match name with alias using hashmap of some kind
+            let menuInfo = new GrubEntryInfo(eName, eName, icon);
+            entries.push(menuInfo);
+        }
+
+        return entries;
+    },
+    _findIcon: function (entryName) {
+        let regExpMap = {
+            'fedora':   [/fedora/i],
+            'windows8': [/windows\s*8/i]
+        };
+
+        let res;
+        mapLoop:
+        for (let k in regExpMap) {
+            let toCheck = regExpMap[k];
+            for (let i = 0; i < toCheck.length; ++i) {
+                let rExp = toCheck[i];
+                if (rExp.exec(entryName)) {
+                    res = k;
+                    break mapLoop;
+                }
+            }
+        }
+
+        if (res)
+            res = new St.Icon({
+                gicon: new Gio.ThemedIcon({name: res}),
+                icon_size: 16
+            })
+
+        return res;
+    }
+});
+
+const GrubEntryInfo = new Lang.Class({
+    Name: 'GrubEntryInfo',
+
+    _init: function (name, alias, icon) {
+        this.name = name;
+        this.alias = alias;
+        this.icon = icon;
+    },
+    destroy: function () {
+    },
+    launch: function (timestamp) {
+        // Set entry as the default grub entry
+        let success, pid;
+        try {
+            [success, pid] = GLib.spawn_async(null, ['pkexec', 'grub2-set-default', this.name], null,
+                                    GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD,
+                                    null);
+        } catch (err) {
+            /* Rewrite the error in case of ENOENT */
+            if (err.matches(GLib.SpawnError, GLib.SpawnError.NOENT)) {
+                throw new GLib.SpawnError({ code: GLib.SpawnError.NOENT,
+                                            message: _("Command not found") });
+            } else {
+                throw err;
+            }
+        }
+
+        GLib.child_watch_add(GLib.PRIORITY_DEFAULT, pid, Lang.bind(this, function () {
+            // Reboot computer
+            this._reboot();
+        }), null);
+    },
+    _reboot: function () {
+        Util.spawn(['reboot']);
+    }
+});
